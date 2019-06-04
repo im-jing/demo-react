e6f9d0535d67197d89f59c5972a2aaad8710c0c6
nancy.zhao <nancy.zhao@nakehub.cn>
2019-06-04 11:14:34 +0800

    add tabs component's unit test

--- src/components/tabs/index.test.jsx
+++ src/components/tabs/index.test.jsx
@@ -3,20 +3,19 @@ import { shallow } from 'enzyme';
 
 import { Tabs, Tab } from './index';
 
+const props = {
+  activeTabIdx: 0,
+};
+
+let wrapper;
+
 describe('Tabs', () => {
-  const props = {
-    activeTabIdx: 0,
-  };
-  // const mockFn = jest.fn();
-
-  // const handleTabClick = new mockFn();
-
-  let wrapper;
   beforeEach(() => {
     wrapper = shallow(
       <Tabs {...props}>
         <Tab>aaaa</Tab>
         <Tab>bbbb</Tab>
+        <Tab>cccc</Tab>
       </Tabs>,
     );
   });
@@ -25,14 +24,49 @@ describe('Tabs', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('tabs has two items', () => {
-    // expect(wrapper.children().length).toEqual(2);
-    expect(wrapper.children()).toHaveLength(2);
+  it('tabs has three items', () => {
+    // expect(wrapper.children().first().find('li')).toEqual(3);
+    expect(wrapper.children().first().find('li')).toHaveLength(3);
   });
 
-  // it('simulate click on the tab', () => {
-  //   const tab = wrapper.children();
-  //   tab.simulate('click');
-  //   expect(tab.text()).toEqual('aaa');
-  // });
+  it('first render the tabs', () => {
+    // console.log(wrapper.debug(), '==wrapper==');
+    const tab = wrapper.children().find('.tabs li');
+    const tabFirst = tab.at(0);
+    const tabSecond = tab.at(1);
+    const tabThird = tab.at(2);
+
+    expect(tabFirst.find('button').hasClass('active')).toEqual(true);
+    expect(tabSecond.find('button').hasClass('active')).toEqual(false);
+    expect(tabThird.find('button').hasClass('active')).toEqual(false);
+    expect(wrapper.state('activeIdx')).toEqual(0);
+  });
+
+  it('simulate click on the second tab', () => {
+    wrapper.setState({ activeIdx: 1 });
+    wrapper.children().find('.tabs li').at(1).simulate('click');
+
+    expect(wrapper.children().find('.tabs li').at(0).find('button')
+      .hasClass('active')).toEqual(false);
+    expect(wrapper.children().find('.tabs li').at(1).find('button')
+      .hasClass('active')).toEqual(true);
+    expect(wrapper.state('activeIdx')).toEqual(1);
+  });
+
+  it('click the second tab, the handleTabClick() should be called', () => {
+    // console.log(wrapper.props(), '===props===');
+    // console.log(wrapper.debug(), '==wrapper==');
+    // console.log(wrapper.instance(), '==wrapper.instance()==');
+    wrapper.instance().handleTabClick = jest.fn();
+    wrapper.children().find('.tabs li').at(1).find('button')
+      .simulate('click');
+    wrapper.setState({ activeIdx: 1 });
+
+    expect(wrapper.instance().handleTabClick).toHaveBeenCalled();
+    expect(wrapper.instance().handleTabClick).toHaveBeenCalledWith(wrapper.state('activeIdx'));
+  });
+
+  afterAll(() => {
+    wrapper.unmount();
+  });
 });
